b7797c58777db5fcc9428e7d78325895bfc49726
Tim Iland <TimIland@driveworks.co.uk>
2021-04-25 14:04:22 +0100

    to remote

---
+++ Root/Client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default App;
--- Root/Client/webpack.config.js
+++ Root/Client/webpack.config.js
@@ -1,1 +1,115 @@
 const isDevelopment = process.env.NODE_ENV === 'development'
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: ' index_bundle.js',
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(scss|css)$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+
+
+
+
+
+
+
+
+
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin()
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',
+      generateStatsFile: true,
+      statsOptions: { source: false }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+};
+
+
+
+// {
+//     test: /\.(jpg|png|gif|svg)$/,
+//     loader: 'image-webpack-loader',
+//     enforce: 'pre'
+//   },
+//   {
+//     test: /\.js$/,
+//     exclude: /node_modules/,
+//     use: {
+//       loader: 'babel-loader',
+//       options: {
+//         presets: [
+//           ['env', {'modules' :false}],
+//           'react'
+//         ]
+//       }
+//     },
+//   },
+//   {
+//     // loader options seemed to do the trick here it obviously needed the css loader and style loader
+//     test: /\.css$/,
+//     use: [
+//       "style-loader",
+//       {
+//         loader: "css-loader",
+//         options: {
+//           importLoaders: 1
+//         }
+//       },
+//       "postcss-loader"
+//     ]
+//   },
+//   {
+//     test: /\.(png|jpg)$/,
+//     use: [
+//       {
+//         loader: 'url-loader',
+//         options: {
+//           esModule: false,
+//           limit: 10 * 1024,
+//           name: '[name].[ext]',
+//           outputPath: 'images/',
+//           publicPath: 'images/',
+//         },
+//       },
+//     ],
+//   },
+//   {
+//     test: /\.(svg)$/,
+//     use: [
+//       {
+//         loader: 'svg-url-loader',
+//         options: {
+//           esModule: false,
+//           limit: 10 * 1024,
+//           name: '[name].[ext]',
+//           outputPath: 'images/',
+//           publicPath: 'images/',
+//           noquotes: true,
+//         },
+//       },
+//     ],
+//   },
